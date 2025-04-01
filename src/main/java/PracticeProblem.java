80a2028a81a7e48e800e48bfc827ece1ea714585
Radin-Ajorlou-ICS4U-02-25 <radin.ajorlou@student.tdsb.on.ca>
2025-04-01 13:58:55 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,37 @@ public class PracticeProblem {
 
 	}
 
+
+
+	public static int sumOfDigits(int x){
+
+		if (x == 0){
+			return 0;
+		}
+
+		if (x < 0){
+			x *= -1;
+		}
+
+		return (x % 10) + sumOfDigits(x /= 10);
+	}
+
+
+
+	public static int factorial(int num){
+		if (num == 0){
+			return 1;
+		}
+
+		if (num < 0){
+			return 0;
+		}
+
+		return factorial(num - 1) * num;
+
+
+	}
+
 	
 
 }
